b89f5449cc70ba0c2b97bc0cf8e4c61a97381cba
Acha-Goodness <acharious1668@gmail.com>
2025-08-15 16:29:01 -0700

    configured cloudinary for image upload

---
+++ Helpers/cloudinary.js
@@ -0,0 +1,23 @@
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
+});
+
+const storage = new multer.memoryStorage();
+
+const imageUploadUtill = async (File) => {
+    const result = await cloudinary.uploader.upload(File, {
+        resource_type : "auto"
+    })
+
+    return result;
+}
+
+const upload = multer({storage})
+
+module.exports = {upload, imageUploadUtill};
