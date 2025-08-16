190cd16399b64f411004b8f53702c8fb5b45b639
Acha-Goodness <acharious1668@gmail.com>
2025-08-16 13:24:12 -0700

    bug fix and code refactor

--- Controllers/messageController.js
+++ Controllers/messageController.js
@@ -9,7 +9,7 @@ exports.getUsersForSidebar = catchAsync ( async (req, res, next) => {
     try{
         const loggedInUserId = req.user._id;
         const filteredUsers = await User.find({ _id: { $ne: loggedInUserId }}).select("-password");
-
+        
         res.status(200).json({
             success: true,
             users: filteredUsers,
--- Models/userModel.js
+++ Models/userModel.js
@@ -62,12 +62,12 @@ userSchema.pre("save", async function(next){
     next();
 });
 
-userSchema.pre("save", function(next){
-    if(!this.isModified("password") || this.isNew) return next();
+// userSchema.pre("save", function(next){
+//     if(!this.isModified("password") || this.isNew) return next();
 
-    this.passwordChangedAt = Date.now() - 1000;
-    next();
-});
+//     this.passwordChangedAt = Date.now() - 1000;
+//     next();
+// });
 
 userSchema.methods.changedPasswordAfter = function(JWTTimestap){
     if(this.passwordChangedAt){
